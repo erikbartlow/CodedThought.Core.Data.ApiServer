069febaa3a300a554d01cd13c4fe0c3f27997795
Erik Bartlow <Ebartlow@versonasystems.com>
2024-11-06 10:44:04 -0600

    Fixed Dependencies to CodedThought.Core and Configuration.

---
+++ GenericApiDataStoreController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CodedThought.Core.Data.ApiServer {
+
+	public class GenericApiDataStoreController : ControllerBase, IDisposable {
+
+		/// <summary>Gets or sets the name of the connection.</summary>
+		/// <value>The name of the connection.</value>
+		public string ConnectionName { get; set; }
+
+		/// <summary>Gets or sets the data store.</summary>
+		/// <value>The data store.</value>
+		public GenericApiDataStore DataStore { get; set; }
+
+		/// <summary>Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.</summary>
+		public void Dispose() {
+			if (DataStore != null) {
+				DataStore = null;
+			}
+		}
+
+        protected override void ExecuteCore() => throw new NotImplementedException();
+    }
+}
